e79f51d9bee99456777217b2c47f605d0d49f946
danielgherghel <d91gherghel@gmail.com>
2024-12-22 13:29:54 -0800

    objects in state

---
+++ Dummy/objectsInState/src/App.jsx
@@ -0,0 +1,64 @@
+import './App.css'
+import { useState } from 'react'
+import avatar from './images/user.jpg'
+import starFilled from './images/filled-star.jpg'
+import starEmpty from './images/star.png'
+
+
+
+function App() {
+
+  
+  const [toggleBtn, setToggleBtn] = useState(false)
+
+  const [ contact, setContact] = useState({
+    firstName: 'Jon',
+    lastName: 'Doe',
+    phone: '+1 (205) 444-1212',
+    email: "email.email.com",
+    isFavorite: false
+  })
+
+
+  function toggleFavorite(){
+    setContact((item) => ({
+      ...item,
+      isFavorite: !item.isFavorite
+    }))
+  }
+
+
+  return (
+    <main>
+      <article className='card'>
+        <img
+          src={avatar}
+          className='avatar'
+          alt='whatever name'
+        ></img>
+        <div className="info">
+          <button
+            onClick={toggleFavorite}
+            aria-pressed={false}
+            className='favorite-button'
+          >
+            <img
+              src={contact.isFavorite ? starFilled : starEmpty}
+              alt='star empty'
+              className='favorite'
+            ></img>
+          </button>
+          <h2 className='name'>
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="contact">{contact.phone}</p>
+          <p className="contact">{contact.email}</p>
+        </div>
+      
+      </article>
+
+    </main>
+  )
+}
+
+export default App
