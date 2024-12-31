b26a9a67fbeca13265d64df1840860b8455bf293
Daniel Gherghel <d91gherghel@gmail.com>
2024-12-30 19:59:13 -0800

    form email retrieve

---
+++ Dummy/formPractice7/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+	function submitForm(e) {
+		e.preventDefault();
+		const newData = new FormData(e.target);
+		const emailData = newData.get('emailInput');
+		console.log(emailData);
+	}
+
+	return (
+		<>
+			<section className="sectionContainer">
+				<form
+					className="formContainer"
+					onSubmit={submitForm}
+				>
+					<h1>Sign up</h1>
+					<label htmlFor="emailInput">Email sign up</label>
+					<input
+						type="email"
+						placeholder="type in your email..."
+						name="emailInput"
+						id="emaiInput"
+					></input>
+					<label>Password</label>
+					<input
+						type="password"
+						placeholder="type in the password"
+						name="passwordInput"
+					></input>
+					<button>Submit</button>
+				</form>
+			</section>
+		</>
+	);
+}
+
+export default App;
